53746c050b63777bd4a16458ff85ed0d3bfd1ea8
mcored <mcored@6e0b5bc6-0638-11de-bc6d-132d5dcf21d4>
2009-03-01 08:16:35 +0000

    Initial Commit

---
+++ trunk/EarthquakeAnalysisTool/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AccelerationTimeHistoryGen
+{
+    public partial class Form1 : Form
+    {
+        private cAthGen mAthGen = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+            if (paths.Length >= 1)
+            {
+                mAthGen = new cAthGen(paths);
+            }
+        }
+
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
