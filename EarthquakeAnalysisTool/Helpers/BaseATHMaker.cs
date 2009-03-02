c94eeae455afe58d7ff2cc8457719a30f4f15c12
mcored <mcored@6e0b5bc6-0638-11de-bc6d-132d5dcf21d4>
2009-03-02 14:46:07 +0000

    ExcelReport.cs to generate ATH

---
+++ EarthquakeAnalysisTool/Helpers/BaseATHMaker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AccelerationTimeHistoryGen
+{
+    /// <summary>
+    /// Reads Shake91 input file and generates an accelerogram in Excel
+    /// </summary>
+    class BaseATHMaker
+    {
+        private string mPath;
+        /// <summary>
+        /// Acceleration Time Histories
+        /// </summary>
+        private List<string> mATH = new List<string>();
+        private int NTPS;
+        private decimal mDT;
+
+        public BaseATHMaker(string fp)
+        {
+            this.mPath = fp;
+        }
+
+        public int DT { get { return (int)(mDT * 1000); } private set { ; } }
+
+        private void RemoveDoubleSpaces(ref string line)
+        {
+            line = line.Trim();
+            while (line.Contains("  "))
+            {
+                line = Regex.Replace(line, "  ", " ");
+            }
+        }
+
+        private List<string> SplitLine(string line)
+        {
+            List<string> acc = new List<string>();
+            string[] nums = Regex.Split(line, " ");
+            foreach (string s in nums)
+            {
+                acc.Add(s);
+            }
+            return acc;
+        }
+
+        private bool IsValidLine(string line)
+        {
+            return SplitLine(line).Count > 0;
+        }
+
+        public List<string> ReadATH()
+        {
+            using (StreamReader sr = new StreamReader(mPath))
+            {
+                mATH.Clear();
+                string line = sr.ReadLine();
+                line = sr.ReadLine();
+                line = sr.ReadLine();
+                line = sr.ReadLine(); // NPTS=  5366, DT= .00500 SEC
+                GroupCollection gc = Regex.Match(line, "NPTS=  (?<NTPS>.+), DT= (?<DT>.+) SEC").Groups;
+                int.TryParse(gc[1].Value, out this.NTPS);
+                decimal.TryParse(gc[2].Value, out this.mDT);
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    RemoveDoubleSpaces(ref line);
+                    foreach (string s in SplitLine(line))
+                    {
+                        mATH.Add(s);
+                        if (mATH.Count == NTPS)
+                        {
+                            return mATH;
+                        }
+                    }
+                }
+
+            }
+
+
+            return mATH;
+        }
+
+
+    }
+}
--- EarthquakeAnalysisTool/MainWindow.cs
+++ EarthquakeAnalysisTool/MainWindow.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
+using AccelerationTimeHistoryGen.Helpers;
 
 namespace AccelerationTimeHistoryGen
 {
@@ -17,32 +18,22 @@ namespace AccelerationTimeHistoryGen
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Form1_DragDrop(object sender, DragEventArgs e)
-        {
-            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
-            if (paths.Length >= 1)
-            {
-                mAthGen = new ATHMaker(paths);
-            }
-        }
+            Control ctl = this.GetNextControl(this, true); // Get the first control in the tab order.
 
-        private void tpATHExcel_DragDrop(object sender, DragEventArgs e)
-        {
-            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
-            if (paths.Length == 1)
+            while (ctl != null)
             {
-                ATHColumnMaker acm = new ATHColumnMaker(paths[0]);
-                acm.MakeColumn(8*(int)nudATHCount.Value);
-                if (File.Exists(acm.ExcelFilePath))
+                if (ctl.GetType() == typeof(TextBox))
                 {
-                    Process.Start(acm.ExcelFilePath);
+                    ctl.AllowDrop = true;
+                    ((TextBox)ctl).DragDrop += new DragEventHandler(TextBox_DragDrop);
+                    ctl.DragEnter += new DragEventHandler(TextBox_DragEnter);
                 }
+                ctl = this.GetNextControl(ctl, true); // Get the next control in the tab order.
             }
         }
 
-        private void tpATHExcel_DragEnter(object sender, DragEventArgs e)
+        void TextBox_DragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -51,6 +42,25 @@ namespace AccelerationTimeHistoryGen
             else
             {
                 e.Effect = DragDropEffects.None;
+            }
+        }
+
+        void TextBox_DragDrop(object sender, DragEventArgs e)
+        {
+            TextBox myTextBox = (TextBox)sender;
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+            if (paths.Length == 1)
+            {
+                myTextBox.Text = paths[0];
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+            if (paths.Length >= 1)
+            {
+                mAthGen = new ATHMaker(paths);
             }
         }
 
@@ -66,7 +76,56 @@ namespace AccelerationTimeHistoryGen
             }
         }
 
+        private void btnBrowseBaseATH_Click(object sender, EventArgs e)
+        {
+            BrowseFile(ref txtATHBaseFile);
+        }
 
+        private void BrowseFile(ref TextBox textBox)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox.Text = dlg.FileName;
+            }
+        }
+
+        private void btnBrowseSurfaceATH_Click(object sender, EventArgs e)
+        {
+            BrowseFile(ref txtATHSurfaceFile);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtExcelFile.Text = dlg.FileName;
+                SurfaceATHMaker acm = new SurfaceATHMaker(txtATHSurfaceFile.Text);
+                acm.MaxValues = 8 * (int)nudATHCount.Value;
+                BaseATHMaker bm = new BaseATHMaker(txtATHBaseFile.Text);
+                ExcelReporter er = new ExcelReporter(txtExcelFile.Text);
+                er.MySurfaceATHMaker = acm;
+                er.MyBaseATHMaker = bm;
+                er.CreateReport();
+
+                if (File.Exists(txtExcelFile.Text))
+                {
+                    Process.Start(txtExcelFile.Text);
+                }
+            }
+
+        }
+
+        private void txtATHBaseFile_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void txtATHBaseFile_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
 
 
     }
