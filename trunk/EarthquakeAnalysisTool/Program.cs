ec568549353af04791873b0cd2cb4545ca68cd8d
mcored <mcored@6e0b5bc6-0638-11de-bc6d-132d5dcf21d4>
2009-03-02 06:23:04 +0000

    0.2.0.0 ATH from Shake91 to Excel graphs 0.1.0.0 Generating ATH for Shake91 Soil Profile calculations (not working)

--- trunk/EarthquakeAnalysisTool/Program.cs
+++ trunk/EarthquakeAnalysisTool/Program.cs
@@ -14,7 +14,7 @@ namespace AccelerationTimeHistoryGen
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
