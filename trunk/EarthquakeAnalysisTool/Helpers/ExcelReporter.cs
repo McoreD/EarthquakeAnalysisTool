70909dd1292c404c68e2c6b72c46e1b3ca6fb1ae
mcored <mcored@6e0b5bc6-0638-11de-bc6d-132d5dcf21d4>
2009-03-05 04:20:32 +0000

    Calculate VTH and DTH from Surface and Base ATH

--- trunk/EarthquakeAnalysisTool/Helpers/ExcelReporter.cs
+++ trunk/EarthquakeAnalysisTool/Helpers/ExcelReporter.cs
@@ -8,6 +8,13 @@ using System.ComponentModel;
 
 namespace AccelerationTimeHistoryGen.Helpers
 {
+    public struct ExcelReporterOptions
+    {
+        public BackgroundWorker Worker { get; set; }
+        public string WorkbookFilePath { get; set; }
+        public bool CalculateDisplacements { get; set; }
+    }
+
     class ExcelReporter
     {
         private string mPath;
@@ -37,16 +44,19 @@ namespace AccelerationTimeHistoryGen.Helpers
             get { return mWSheet1; }
         }
 
+        private ExcelReporterOptions Options { get; set; }
+
         int startRow = 3;
 
         /// <summary>
         /// Filepath of the Excel Report file
         /// </summary>
         /// <param name="fp"></param>
-        public ExcelReporter(BackgroundWorker bwApp, string fp)
+        public ExcelReporter(ExcelReporterOptions options)
         {
-            this.mPath = fp;
-            this.mBwApp = bwApp;
+            this.Options = options;
+            this.mPath = options.WorkbookFilePath;
+            this.mBwApp = options.Worker;
         }
 
         /// <summary>
@@ -59,16 +69,18 @@ namespace AccelerationTimeHistoryGen.Helpers
             //************************
 
             mExcelApp.DisplayAlerts = false;
-            mWorkBook = mExcelApp.Workbooks.Open(mXlsFilePath, 0, false, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+            //mWorkBook = mExcelApp.Workbooks.Open(mXlsFilePath, 0, false, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            mWorkBook = mExcelApp.Workbooks.Add(Missing.Value);
 
             mWorkSheets = mWorkBook.Worksheets;
             mWSheet1 = (Microsoft.Office.Interop.Excel.Worksheet)mWorkSheets.get_Item("Sheet1");
 
             FillATHData(mWSheet1);
 
-            GenerateChart(mWSheet1);
+            GenerateCharts(mWSheet1);
 
-            AutofitColumns(mWSheet1, "A1", "E1");
+            AutofitColumns(mWSheet1, "A1", "L1");
 
             string ext = Path.GetExtension(mPath);
 
@@ -90,44 +102,172 @@ namespace AccelerationTimeHistoryGen.Helpers
             List<string> accBase = MyBaseATHMaker.ReadATH();
             int dtBase = MyBaseATHMaker.DT;
 
-            mBwApp.ReportProgress(0, accBase.Count);
-            mBwApp.ReportProgress(2, "Filling Base ATH...");
+            mBwApp.ReportProgress(0, (this.Options.CalculateDisplacements ? 10 : 4));
+            mBwApp.ReportProgress(2, "Filling Base ATH, VTH and DTH...");
+
+            double[,] arrData = new double[accBase.Count, 1];
+            string[,] arrString = new string[accBase.Count, 1];
+
+            Range headings = ws.get_Range("A1", "B12");
+            headings.Font.Bold = true;
+
+            ws.Cells[1, 1] = "Base";
+
+            // Times
+            ws.Cells[2, 1] = "Time (s)";
+            Range rTime = ws.get_Range(string.Format("A{0}", startRow), string.Format("A{0}", startRow + accBase.Count - 1));
             for (int i = 0; i < accBase.Count; i++)
             {
-                ws.Cells[i + startRow, 1] = (double)(i * dtBase / 1000.0);
-                ws.Cells[i + startRow, 2] = accBase[i];
+                arrData[i, 0] = (double)(i * dtBase / 1000.0);
+            }
+            rTime.Value2 = arrData;
+            mBwApp.ReportProgress(1);
+
+            // ATH (G)
+            ws.Cells[2, 2] = "Accel (g)";
+            Range rAthG = ws.get_Range(string.Format("B{0}", startRow), string.Format("B{0}", startRow + accBase.Count - 1));
+            for (int i = 0; i < accBase.Count; i++)
+            {
+                double value;
+                double.TryParse(accBase[i], out value);
+                arrData[i, 0] = value;
+            }
+            rAthG.Value2 = arrData;
+            mBwApp.ReportProgress(1);
+
+            if (this.Options.CalculateDisplacements)
+            {
+                // ATH (m/ss)
+                ws.Cells[2, 3] = "Accel (m/ss)";
+                Range rAth = ws.get_Range(string.Format("C{0}", startRow), string.Format("C{0}", startRow + accBase.Count - 1));
+                for (int i = 0; i < accBase.Count; i++)
+                {
+                    arrString[i, 0] = "=RC[-1]*9.81";
+                }
+                rAth.FormulaArray = arrString;
                 mBwApp.ReportProgress(1);
+
+                // VTH (m/s)
+                ws.Cells[2, 4] = "veloc (m/s)";
+                Range rVth = ws.get_Range(string.Format("D{0}", startRow + 1), string.Format("D{0}", startRow + accBase.Count - 1));
+                for (int i = 0; i < accBase.Count; i++)
+                {
+                    arrString[i, 0] = "=R[-1]C+0.5*(R[-1]C[-1]+RC[-1])*(RC[-3]-R[-1]C[-3])";
+                }
+                rVth.FormulaArray = arrString;
+                rVth.Style = "Calculation";
+                mBwApp.ReportProgress(1);
+
+                // DTH (m)
+                ws.Cells[2, 5] = "disp (m)";
+                Range rDth = ws.get_Range(string.Format("E{0}", startRow + 1), string.Format("E{0}", startRow + accBase.Count - 1));
+                for (int i = 0; i < accBase.Count; i++)
+                {
+                    arrString[i, 0] = "=R[-1]C+R[-1]C[-1]*(RC[-4]-R[-1]C[-4])+0.5*R[-1]C[-3]*(RC[-4]-R[-1]C[-4])^2";
+                }
+                rDth.FormulaArray = arrString;
+                rDth.Style = "Output";
+                mBwApp.ReportProgress(1);
+
             }
 
             List<string> accSurface = MySurfaceATHMaker.ReadATH();
             double dt = ((double)accBase.Count / (double)accSurface.Count) * dtBase;
 
-            mBwApp.ReportProgress(0, accSurface.Count);
-            mBwApp.ReportProgress(2, "Filling Surface ATH...");
+            arrData = new double[accSurface.Count, 1];
+            arrString = new string[accSurface.Count, 1];
+
+            mBwApp.ReportProgress(2, "Filling Base ATH, VTH and DTH...");
+
+            ws.Cells[1, 8] = "Surface";
+
+            // Times
+            ws.Cells[2, 8] = "Time (s)";
+            Range rTimeS = ws.get_Range(string.Format("H{0}", startRow), string.Format("H{0}", startRow + accSurface.Count - 1));
             for (int i = 0; i < accSurface.Count; i++)
             {
-                ws.Cells[i + startRow, 4] = (double)(i * dt / 1000.0);
-                ws.Cells[i + startRow, 5] = accSurface[i];
+                arrData[i, 0] = (double)(i * dt / 1000.0);
+            }
+            rTimeS.Value2 = arrData;
+            mBwApp.ReportProgress(1);
+
+            // ATH (G)
+            ws.Cells[2, 9] = "Accel (g)";
+            Range rAthGS = ws.get_Range(string.Format("I{0}", startRow), string.Format("I{0}", startRow + accSurface.Count - 1));
+            for (int i = 0; i < accSurface.Count; i++)
+            {
+                double value;
+                double.TryParse(accSurface[i], out value);
+                arrData[i, 0] = value;
+            }
+            rAthGS.Value2 = arrData;
+            mBwApp.ReportProgress(1);
+
+            if (this.Options.CalculateDisplacements)
+            {
+                // ATH (m/ss)
+                ws.Cells[2, 10] = "Accel (m/ss)";
+                Range rAthS = ws.get_Range(string.Format("J{0}", startRow), string.Format("J{0}", startRow + accSurface.Count - 1));
+                for (int i = 0; i < accSurface.Count; i++)
+                {
+                    arrString[i, 0] = "=RC[-1]*9.81";
+                }
+                rAthS.FormulaArray = arrString;
                 mBwApp.ReportProgress(1);
+
+                // VTH (m/s)
+                ws.Cells[2, 11] = "veloc (m/s)";
+                Range rVthS = ws.get_Range(string.Format("K{0}", startRow + 1), string.Format("K{0}", startRow + accSurface.Count - 1));
+                for (int i = 0; i < accSurface.Count; i++)
+                {
+                    arrString[i, 0] = "=R[-1]C+0.5*(R[-1]C[-1]+RC[-1])*(RC[-3]-R[-1]C[-3])";
+                }
+                rVthS.FormulaArray = arrString;
+                rVthS.Style = "Calculation";
+                mBwApp.ReportProgress(1);
+
+                // DTH (m)
+                ws.Cells[2, 12] = "disp (m)";
+                Range rDthS = ws.get_Range(string.Format("L{0}", startRow + 1), string.Format("L{0}", startRow + accSurface.Count - 1));
+                for (int i = 0; i < accSurface.Count; i++)
+                {
+                    arrString[i, 0] = "=R[-1]C+R[-1]C[-1]*(RC[-4]-R[-1]C[-4])+0.5*R[-1]C[-3]*(RC[-4]-R[-1]C[-4])^2";
+                }
+                rDthS.FormulaArray = arrString;
+                rDthS.Style = "Output";
+                mBwApp.ReportProgress(1);
+
             }
 
             mBwApp.ReportProgress(2, "Ready");
 
         }
 
-        private void GenerateChart(Worksheet ws)
+        //private void FillRangeFormula(Worksheet ws, string column, int rows, string data)
+        //{
+        //    Range range = ws.get_Range("
+        //    string[,] arrData = new string[range.Count, 1];
+        //    for (int i = 0; i < arrData.Length; i++)
+        //    {
+        //        arrData[i, 0] = data;
+        //    }
+        //    range.FormulaArray = data;
+        //}
+
+        private void GenerateCharts(Worksheet ws)
         {
             try
             {
                 // Now create the chart.
 
-                Chart xlChart = (Chart)mWorkBook.Charts.Add(Missing.Value, Missing.Value,
-                                                            Missing.Value, Missing.Value);
+                // Chart xlChart = (Chart)mWorkBook.Charts.Add(Missing.Value, Missing.Value,Missing.Value, Missing.Value);
+                ChartObjects chartObjs = (ChartObjects)ws.ChartObjects(Type.Missing);
+                ChartObject chartObj = chartObjs.Add(300, 20, 960, 540);
+                Chart xlChart = chartObj.Chart;
 
-                //ChartObjects chartObjs = (ChartObjects)ws.ChartObjects(Type.Missing);
-                //ChartObject chartObj = chartObjs.Add(300, 20, 960, 540);
-                // Chart xlChart = chartObj.Chart;
+
                 SeriesCollection seriesCollection = (SeriesCollection)xlChart.SeriesCollection(Type.Missing);
+
                 seriesCollection.NewSeries();
                 seriesCollection.Item(1).Name = "Base";
                 seriesCollection.Item(1).XValues = string.Format("=Sheet1!$A{0}:$A{1}", startRow, MyBaseATHMaker.ATH.Count);
@@ -135,8 +275,8 @@ namespace AccelerationTimeHistoryGen.Helpers
 
                 seriesCollection.NewSeries();
                 seriesCollection.Item(2).Name = "Surface";
-                seriesCollection.Item(2).XValues = string.Format("=Sheet1!$D{0}:$D{1}", startRow, MySurfaceATHMaker.ATH.Count);
-                seriesCollection.Item(2).Values = string.Format("=Sheet1!$E{0}:$E{1}", startRow, MySurfaceATHMaker.ATH.Count);
+                seriesCollection.Item(2).XValues = string.Format("=Sheet1!R{0}C8:R{1}C8", startRow, MySurfaceATHMaker.ATH.Count);
+                seriesCollection.Item(2).Values = string.Format("=Sheet1!R{0}C9:R{1}C9", startRow, MySurfaceATHMaker.ATH.Count);
 
                 xlChart.ChartType = XlChartType.xlXYScatterSmoothNoMarkers;
 
@@ -156,6 +296,10 @@ namespace AccelerationTimeHistoryGen.Helpers
                 xlChart.ChartTitle.Text = "Acceleration Time History";
                 // Remove legend:
                 xlChart.HasLegend = true;
+
+
+                xlChart.Location(XlChartLocation.xlLocationAsNewSheet, "ATH (Base vs. Surface)");
+
             }
             catch (Exception ex)
             {
@@ -180,8 +324,6 @@ namespace AccelerationTimeHistoryGen.Helpers
                              Missing.Value,
                              Missing.Value);
 
-            mExcelApp.DisplayAlerts = true;
-
             return filePath;
         }
 
@@ -205,8 +347,6 @@ namespace AccelerationTimeHistoryGen.Helpers
                              Missing.Value,
                              Missing.Value);
 
-            mExcelApp.DisplayAlerts = true;
-
             return filePath;
         }
 
@@ -217,7 +357,6 @@ namespace AccelerationTimeHistoryGen.Helpers
 
         private void AutofitColumns(Worksheet ws, string colStart, string colFinish)
         {
-            //AutoFit columns A:D.
             Microsoft.Office.Interop.Excel.Range oRng;
             oRng = ws.get_Range(colStart, colFinish);
             oRng.EntireColumn.AutoFit();
