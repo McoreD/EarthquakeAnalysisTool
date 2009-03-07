a7ac8af0ad920b80fc0211dc1416a81da41d7d77
mcored <mcored@6e0b5bc6-0638-11de-bc6d-132d5dcf21d4>
2009-03-07 16:05:36 +0000

    mExcelApp.DisplayAlerts = true;

--- trunk/EarthquakeAnalysisTool/Helpers/ExcelReporter.cs
+++ trunk/EarthquakeAnalysisTool/Helpers/ExcelReporter.cs
@@ -104,6 +104,8 @@ namespace THTool.Helpers
                 ExcelFilePath = SaveAs2003(mPath);
             }
 
+            mExcelApp.DisplayAlerts = true;
+
             mExcelApp.Quit();
 
         }
